fca34b055efce6875cce45be1f2357d11abd6b22
sahithiperumalla <109752174+sahithiperumalla@users.noreply.github.com>
2025-08-19 08:48:53 +0530

    Add files via upload

---
+++ nurseryapp/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import First from './components/First';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <First />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
